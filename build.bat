9f58a60f67ae8d44d25a86467de665b6312a7857
Marky-D-32 <157210765+Marky-D-32@users.noreply.github.com>
2026-06-05 13:11:44 +0100

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,8 @@
+SET XROARPATH=C:\Users\Marky\Documents\Emulators\Dragon32\apps\xroar-1.6.3-w64
+SET ASMPATH=C:\Users\Marky\Documents\Emulators\Dragon32\apps\asm6809-2.12-w64
+
+SET path=%XROARPATH%;%ASMPATH%
+
+asm6809.exe --exec 28000 --dragondos Cupid.asm -o Cupid.bin -l Cupid.lst
+
+xroar.exe -default-machine d32 -rompath %XROARPATH% -run Cupid.bin -cart Alldream cart-desc "Alldream" cart-type rom cart-rom "../../cartridges/AllDream.rom" no-cart-autorun
